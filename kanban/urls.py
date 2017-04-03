7155c18217621d0741c116892a57f8f663beabc5
Ryan Semmler <semmra7@gmail.com>
2017-04-03 17:09:19 -0400

    update

--- kanban/urls.py
+++ kanban/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    url(r'^', include('tasks.urls')),
+    url(r'^', include('static/index.html')),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('django.contrib.auth.urls')),
 ]
