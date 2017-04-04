dcf586511eea4cc27f1f6654b0fb32a4d700967f
Ryan Semmler <semmra7@gmail.com>
2017-04-04 09:53:57 -0400

    commit

--- kanban/static/js/kanban.js
+++ kanban/static/js/kanban.js
@@ -1,6 +1,6 @@
 //js file for kanban hw
 
-$(document).ready(function() {
+// $(document).ready(function() {
 
     $('#allTasks').click(function(event) {
         console.log("HI");
@@ -10,12 +10,20 @@ $(document).ready(function() {
           url: 'api/tasks/',
           success: function(result) {
              console.log("success");
-             $box = $('<div>');
-             $($box).append(result);
-           }
-        $('#allTasks').empty();
-        $('#allTasks').append('<h2>All Tasks</h2>');
-        $('#allTasks').append($box);
+
+             $list = $('<ol>');
+
+             for(var i = 0; i < result.length; i++) {
+                console.log(result[i]);
+                $listItem = $('<li>');
+                $listItem.html(result[i].title);
+                $list.append($listItem);
+             }
+
+      //  $('#allTasks').empty();
+    //    $('#allTasks').append('<h2>All Tasks</h2>');
+        $('#tasklist').append($list);
+      }
     });
   })
-});
+// });
