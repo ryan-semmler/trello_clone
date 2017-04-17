19898cd7378ce19f66dfd0249cc16118aa5acb8b
Ryan Semmler <semmra7@gmail.com>
2017-04-17 14:44:43 -0400

    updated

--- kanban/settings.py
+++ kanban/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '*0_0dexkvf1&!v0cdo5*1=js5=_m91bnvyc@b%lzf^qg7dh-!4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tranquil-fortress-47789.herokuapp.com', '0.0.0.0', ]
+ALLOWED_HOSTS = ['tranquil-fortress-47789.herokuapp.com', '0.0.0.0', '127.0.0.1']
 
 
 # Application definition
