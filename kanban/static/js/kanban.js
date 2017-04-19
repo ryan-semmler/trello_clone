fcbf055240e74c1565224b90fd4774a122063e0e
Will Warren <will.warren04@gmail.com>
2017-04-19 16:38:42 -0400

    updates

--- kanban/static/js/kanban.js
+++ kanban/static/js/kanban.js
@@ -20,7 +20,7 @@ $('#allTasks').click(function(event) {
             $itemPriority = result[i].priority;
             $task = $itemTitle + " [" + $itemStatus.slice(4, -1) + "] [" + $itemPriority.slice(4,-1) + ']'
             //add task to list item
-            $listItem.append($task).append('<img class=editTask src="static/img/edit.png"/>').append('<img class=delTask src="/static/img/garbage.png"/></li>');
+            $listItem.append($task).append('<img class=editTask src="static/img/edit.png"/>').append('<a href="/"<img class=delTask src="/static/img/garbage.png"/></li>');
             //add list item to list
             $list.append($listItem);
         }
@@ -34,19 +34,33 @@ $('#allTasks').click(function(event) {
 });
 
 //function to delete a task on click of the rubbish bin
-$('#allTasks').on('click', '.delTask', function(event){
-    $parentLi = $('.delTask').parent();
-    $liId = $parentLi.attr('id').slice(-1);
-    console.log('del');
+// $('#allTasks').on('click', '.delTask', function(event){
+//     $parentLi = $('.delTask').parent();
+//     $liId = $parentLi.attr('id').slice(-1);
+//     console.log('del');
+//
+//     $.ajax({
+//         method: 'DELETE',
+//         url: '/api/tasks/' + $liId,
+//         success: function(results) {
+//             console.log('deleted');
+//         }
+//       })
+//     });
 
-    $.ajax({
-        method: 'DELETE',
-        url: '/api/tasks/' + $liId,
-        success: function(results) {
-            console.log('deleted');
+
+//stuff i think i need somewhere
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
         }
-      })
-    });
+    }
+});
 
 //function to edit a task on click of pencil
 $('#allTasks').on('click', ".editTask", function(event) {
