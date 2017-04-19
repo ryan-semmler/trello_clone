b119fec5976e522c57768eedeb1d8296ed5b3b6f
Will Warren <will.warren04@gmail.com>
2017-04-19 14:53:06 -0400

    updating again

--- kanban/static/js/kanban.js
+++ kanban/static/js/kanban.js
@@ -12,58 +12,53 @@ $('#allTasks').click(function(event) {
       success: function(result) {
          for(var i = 0; i < result.length; i++) {
              $listItem = $('<li>');
-
             //just the title
             $listItem.attr('id', 'li_' + result[i].id)
             $itemTitle = result[i].title;
             //task details.
-            console.log($itemTitle);
             $itemStatus = result[i].status;
             $itemPriority = result[i].priority;
             $task = $itemTitle + " [" + $itemStatus.slice(4, -1) + "] [" + $itemPriority.slice(4,-1) + ']'
-            console.log($task);
+            //add task to list item
             $listItem.append($task).append('<img class=editTask src="static/img/edit.png"/>').append('<img class=delTask src="/static/img/garbage.png"/></li>');
-            console.log($listItem);
-            // $listItem.append("</li>");
-            console.log($listItem)
+            //add list item to list
             $list.append($listItem);
         }
+        //add list to div
         $('#allTasks').append($list);
 
     }
 });
-
-    //function to delete a task on click of the rubbish bin
-    $('.deleteTask').click(function(event){
-        event.preventDefault();
-        $parentLi = $('.deleteTask').parent();
-        $liId = $parentLi.attr('id').slice(-1);
-        console.log($liId);
-
-        $.ajax({
-            method: 'DELETE',
-            url: '/api/tasks/' + $liId,
-            success: function(results) {
-                console.log('deleted');
-            }
-        })
-    });
-
-    //function to edit a task on click of pencil
-    $('.editTask').click(function(event){
-        event.preventDefault();
-        $parentLi = $('.editTask').parent();
-        $liId = $parentLi.attr('id').slice(-1);
-        console.log($liId);
-
-        $.ajax({
-            method: 'PUT',
-            url: '/api/tasks/' + $liId,
-            success: function(results) {
-                console.log("editted");
-            }
-        })
-    });
-
 $('#allTasks').off('click')
+});
+
+//function to delete a task on click of the rubbish bin
+$('.delTask').click(function(event){
+    $parentLi = $('.delTask').parent();
+    $liId = $parentLi.attr('id').slice(-1);
+    console.log($liId);
+
+    $.ajax({
+        method: 'DELETE',
+        url: '/api/tasks/' + $liId,
+        success: function(results) {
+            console.log('deleted');
+        }
+    })
+});
+
+//function to edit a task on click of pencil
+$('.editTask').click(function(event) {
+    event.preventDefault();
+    $parentLi = $('.editTask').parent();
+    $liId = $parentLi.attr('id').slice(-1);
+    console.log($liId);
+
+    $.ajax({
+        method: 'PUT',
+        url: '/api/tasks/' + $liId,
+        success: function(results) {
+            console.log("editted");
+        }
+    })
 });
