d8a0dc6cf291fca36973d043eb7394f9629cf871
Ryan Semmler <semmra7@gmail.com>
2017-04-20 01:35:29 -0400

    style changes to navbar

--- kanban/settings.py
+++ kanban/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
   'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'kanban',
-        'USER': 'willwarren',
+        'USER': 'raleigh',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
